8d7f9dfa08720bec5aabb776bd8a9ef5691a71a4
Vadym Tereshchuk <link.biz@ukr.net>
2020-01-08 15:12:01 +0200

    Adding class

---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
